ca5d83902fae512b58ba8c87233c11530dd1605e
jithish <63664628+jithish-sekar@users.noreply.github.com>
2020-07-15 23:50:55 +0530

    make it better

--- server.js
+++ server.js
@@ -38,4 +38,8 @@ app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === 'production'){
+ app.use(express.static('client/build')) 
+}
+
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
